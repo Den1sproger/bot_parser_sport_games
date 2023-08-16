05fb0478090aab9e5fe6a57bc70d0284ec9ed26c
Den1sproger <114018798+Den1sproger@users.noreply.github.com>
2023-08-16 15:57:40 +0300

    feat: add selecting tourn types in send message

--- telegram_bot/keyboards/__init__.py
+++ telegram_bot/keyboards/__init__.py
@@ -1,7 +1,6 @@
 from .inline import (get_ikb_gs_url,
                      get_tourn_type_ikb,
                      get_select_tourn_type_ikb,
-                     get_start_mail_ikb,
                      confirm_finish_ikb,
                      confirm_reset_rating)
 from .static import main_kb
